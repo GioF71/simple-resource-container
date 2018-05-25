62565205c6d2c19b5226d483ddd42be6c8ed803c
FulcoGiovanni <FulcoGiovanni@SPD-FULCOGI.office.corp.sia.it>
2018-05-25 08:53:48 +0200

    cleanup

--- src/test/java/eu/giof71/resourceContainer/BasicTest.java
+++ src/test/java/eu/giof71/resourceContainer/BasicTest.java
@@ -11,10 +11,10 @@ import eu.giof71.resourceContainer.simple.SimpleResourceContainer;
 public class BasicTest {
 
 	class Type01 {
-		
+
 		private final int a;
 		private final String b;
-		
+
 		public Type01() {
 			this.a = new Random().nextInt(1000);
 			this.b = UUID.randomUUID().toString();
@@ -28,12 +28,12 @@ public class BasicTest {
 			return b;
 		}
 	}
-	
+
 	class Type02 {
 
 		private final int a;
 		private final String b;
-		
+
 		public Type02() {
 			this.a = new Random().nextInt(1000);
 			this.b = UUID.randomUUID().toString();
@@ -58,7 +58,7 @@ public class BasicTest {
 		Assert.assertEquals(rc.size(), 1);
 		Assert.assertEquals(rc.sizeOf(Type01.class), 1);
 		Assert.assertEquals(rc.sizeOf(name01), 1);
-		
+
 		rc.put(new Type01(), name02, Type01.class);
 		Assert.assertEquals(rc.size(), 2);
 		Assert.assertEquals(rc.sizeOf(Type01.class), 2);
@@ -68,7 +68,7 @@ public class BasicTest {
 		Assert.assertEquals(rc.getList(Type01.class).size(), 2);
 		Assert.assertEquals(rc.getList(name01).size(), 1);
 		Assert.assertEquals(rc.getList(name02).size(), 1);
-		
+
 		rc.put(new Type02(), name02, Type02.class);
 
 		Assert.assertEquals(rc.getList(Type01.class).size(), 2);
@@ -76,13 +76,13 @@ public class BasicTest {
 		Assert.assertEquals(rc.getList(name01).size(), 1);
 		Assert.assertEquals(rc.getList(name02).size(), 2);
 	}
-	
+
 	@Test
 	public void removeOne() {
 		final SimpleResourceContainer c = new SimpleResourceContainer();
 		final Type01 item = c.put(new Type01(), "1", Type01.class);
 		final Type01 removed = c.remove("1", Type01.class);
-		
+
 		Assert.assertSame(item, removed);
 	}
 
@@ -91,16 +91,15 @@ public class BasicTest {
 		final SimpleResourceContainer c = new SimpleResourceContainer();
 		final String resourceName = UUID.randomUUID().toString();
 		c.put(new Type01(), resourceName, Type01.class);
-		
+
 		final Type01 newItem = new Type01();
 		c.put(newItem, resourceName, Type01.class);
-		
+
 		final Type01 checkReplaced = c.get(resourceName, Type01.class);
 		Assert.assertSame(newItem, checkReplaced);
-		
+
 		Assert.assertEquals(1, c.size());
 		Assert.assertEquals(1, c.sizeOf(Type01.class));
 		Assert.assertEquals(1, c.sizeOf(resourceName));
 	}
-
 }
